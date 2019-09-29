9a0cc6360ed204d86666820e9ac3505e0fd5eca2
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-09-29 20:43:45 +0200

    ajout Route-Message & Message-Controller, fin vidéo 6 Ok

--- apiRouter.js
+++ apiRouter.js
@@ -1,7 +1,8 @@
 // Imports
-var express = require('express');
-var usersCtrl = require('./routes/usersCtrl');
-
+var express 	 = require('express');
+var usersCtrl 	 = require('./routes/usersCtrl');
+var messagesCtrl = require('./routes/messagesCtrl');
+ 
 // Router
 exports.router = (function() {
 	var apiRouter = express.Router();
@@ -11,6 +12,10 @@ exports.router = (function() {
 	apiRouter.route('/users/login/').post(usersCtrl.login);
 	apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
 	apiRouter.route('/users/me').put(usersCtrl.updateUserProfile);
+
+	// Messages Routes
+	apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+	apiRouter.route('/messages/').get(messagesCtrl.listMessages);
 	
 	return apiRouter;
 })();
